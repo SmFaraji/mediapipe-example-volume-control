f7c82811b23d746a78b52f4f5120384ccdb96e5e
mohammad faraji <87098671+SmFaraji@users.noreply.github.com>
2023-07-21 13:26:45 +0330

    Add files via upload

---
+++ HandTrackingModule.py
@@ -0,0 +1,45 @@
+#our projects channel -> t.me/EngineeringLab
+#aparat channel       -> www.aparat.com/EngineeringLab
+#youtube channel      -> https://www.youtube.com/channel/UC3aF-Xo0DYh6usCC7yUsWBg
+import cv2
+import mediapipe as mp
+import time
+
+class handDetector():
+    def __init__ (self, mode = False, maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
+                                        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+  
+        if self.results.multi_hand_landmarks: #if there is the hand
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+        return img
+ 
+    def findPosition(self, img, handNo=0, draw=True):
+            
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape # high and width of my webcam
+                cx, cy = int(lm.x*w), int(lm.y*h) #get High and Width of each 21 points of hand
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        return lmList
+
+
